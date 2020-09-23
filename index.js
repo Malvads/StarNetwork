058234fa4b192914ce86c3c6230b635df154fb07
Malvads <thegexi@gmail.com>
2020-09-23 23:29:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const WebSocket = require('ws')
 const Express = require('express')
 const fs = require('fs')
 const satanizeHTML = require('sanitize-html')
+const { uuid } = require('uuidv4')
 const App = Express()
-
-process.on('uncaughtException', err => {})
 
 class ProtoParser {
     static writeString(packet, string){
@@ -21,23 +20,45 @@ class ProtoParser {
         }
         return {res, offset}
     }
-    static parseIncomingMessage(view, offset){
-        let data = ProtoParser.readString(view, offset)
-        const name = data.res
-        offset = data.offset
-        data = ProtoParser.readString(view, offset)
-        const message = data.res
-        offset = data.offset
-        return {name, message}
+    static parseIncomingMessage(view, offset, isPrivate){
+        if(!isPrivate){
+            let data = ProtoParser.readString(view, offset)
+            const name = data.res
+            offset = data.offset
+            data = ProtoParser.readString(view, offset)
+            const message = data.res
+            offset = data.offset
+            return {name, message}
+        } else {
+            let data = ProtoParser.readString(view, offset)
+            const name = data.res
+            offset = data.offset
+            data = ProtoParser.readString(view, offset)
+            const message = data.res
+            offset = data.offset
+            data = ProtoParser.readString(view, offset)
+            const socketTarget = data.res
+            offset = data.offset
+            return {name, message, socketTarget}
+        }
     }
-    static repackAndPreventXSS(msg){
-        let data = ProtoParser.parseIncomingMessage(new DataView(new Uint8Array(msg).buffer), 1)
+    static repackAndPreventXSS(msg, senderId, isPrivate){
+        let data = ProtoParser.parseIncomingMessage(new DataView(new Uint8Array(msg).buffer), 1, isPrivate)
         data.name = satanizeHTML(data.name)
         data.message = satanizeHTML(data.message)
         let packet = [0]
         packet = ProtoParser.writeString(packet, data.name)
         packet = ProtoParser.writeString(packet, data.message)
-        return new Uint8Array(packet)
+        packet = ProtoParser.writeString(packet, senderId)
+        if(!isPrivate){
+            return new Uint8Array(packet)
+        } else {
+            packet[0] = 1
+            return {
+                packet: new Uint8Array(packet),
+                targetSocket: data.socketTarget
+            }
+        }
     }
 }
 
@@ -56,7 +77,7 @@ class HexDumper {
 class ChatServer {
     constructor(port){
         this.port = port
-        this.sockets = []
+        this.sockets = {}
         this.server = null
         this.init()
     }
@@ -65,24 +86,37 @@ class ChatServer {
             port: this.port
         })
         this.server.on('connection', socket => {
-            socket.id = Math.floor(Math.random() * 999999)
-            this.sockets.push(socket)
+            const userId = uuid()
+            socket.userid = userId
+            this.sockets[socket.userid] = socket
             socket.on('message', msg => {
-                const id = socket.id
-                console.log(`Got New Message From: ${socket._socket.remoteAddress}, HexDump [${HexDumper.ArrayToHexDump(msg)}]`)
-                msg = ProtoParser.repackAndPreventXSS(msg)
-                for(let i = 0; i < this.sockets.length; i++){
-                    if(this.sockets[i].id != id && this.sockets[i]._socket.remoteAddress != undefined){
-                        this.sockets[i].send(msg)
-                        console.log(`Server Sended Message to : ${this.sockets[i]._socket.remoteAddress}, HexDump [${HexDumper.ArrayToHexDump(msg)}]`)
-                    }
+                switch(msg[0]){
+                    case 0:
+                        const _id = socket.userid
+                        console.log(`Got New Message From: ${socket._socket.remoteAddress}, HexDump [${HexDumper.ArrayToHexDump(msg)}]`)
+                        msg = ProtoParser.repackAndPreventXSS(msg, _id.toString())
+                        for(let id in this.sockets){
+                            if(this.sockets[id].userid != _id && this.sockets[id]._socket.remoteAddress != undefined){
+                                this.sockets[id].send(msg)
+                                console.log(`Server Sended Message to : ${this.sockets[id]._socket.remoteAddress}, HexDump [${HexDumper.ArrayToHexDump(msg)}]`)
+                            }
+                        }
+                        break
+                    case 1:
+                        console.log(`Got New Message From: ${socket._socket.remoteAddress}, HexDump [${HexDumper.ArrayToHexDump(msg)}]`)
+                        msg = ProtoParser.repackAndPreventXSS(msg, socket.userid.toString(), 1) // 1 means private Message, so send only to 1 socket
+                        const targetSocket = msg.targetSocket
+                        const messageBytes = msg.packet
+                        if(this.sockets[targetSocket]){
+                            this.sockets[targetSocket].send(messageBytes)
+                            console.log(`Socket with id: ${socket.userid} and ip ${socket._socket.remoteAddress} Asked to Send Packet[${HexDumper.ArrayToHexDump(messageBytes)}] to Socket ${this.sockets[targetSocket]._socket.remoteAddress} with Id: ${targetSocket} Server will try to broadcast the message.`)
+                        }
+                        break
                 }
             })
             socket.on('close', function(){
-                for(let i = 0; i < this.sockets.length; i++){
-                    if(socket.id == this.sockets[i].id) this.sockets.splice(i, 1)
-                }
-            })
+                delete this.sockets[userId] 
+            }.bind(this))
         })
     }
 }
